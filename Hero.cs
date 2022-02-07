d104f46a11f1091f76f5156f1404aa933d030bfa
unknown <shoqsl@gmail.com>
2022-02-07 03:21:06 -0600

    Initial classes.

---
+++ Hero.cs
@@ -0,0 +1,17 @@
+public class Hero
+{
+    public string HeroName;
+    public int CharacterPoints;
+    public int ExperiencePoints;
+    public int TotalPoints;
+
+    public int Characteristics, Skills, Perks, Talents, MartialArts, Powers;
+
+    public Skill Acrobatics, Acting, Animal Handler, AutofireSkills, Breakfall, Bribery, Bugging, Bureaucratics,
+    Charm, Climbing, CombatDriving, CombatPiloting, ComputerProgramming, Concealment, Contortionist, Conversation,
+    Cramming, Criminology, Cryptography, Deduction, DefenseManeuver, Demolitions, Disguise, Electronics, FastDraw,
+    ForensicMedicine, Forgery, Gambling, HighSociety, Interrogation, Inventor, Lipreading, Lockpicking, Mechanics,
+    Mimicry, Navigation, Oratory, Paramedics, Persuasion, RapidAttack, Riding, SecuritySystems, Shadowing, SleightOfHand,
+    Stealth, Streetwise, Survival, SystemsOperation, Tactics, Teamwork, Trading, TransportFamiliarity, TwoWeaponFighting,
+    Ventriloquism, Weaponsmith;
+}
---
+++ Skill.cs
@@ -0,0 +1,7 @@
+public class Skill
+{
+    public string SkillName;
+    public string SkillCharacteristic;
+    public int Value;
+
+}
